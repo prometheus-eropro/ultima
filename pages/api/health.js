f5faed6591f5d8fc822027b4951b42ce74f2c5df
prometheus-eropro <assistentes.prometheus@gmail.com>
2025-09-14 16:48:22 -0300

    Delete pages directory

--- pages/api/health.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    baseId: !!process.env.AIRTABLE_BASE_ID,
-    token: !!process.env.AIRTABLE_TOKEN,
-    node: process.version,
-    now: new Date().toISOString()
-  });
-}
