e1e6e0c9f060382f19472f39ea94a5fedc3bad51
prometheus-eropro <assistentes.prometheus@gmail.com>
2025-09-16 17:13:50 -0300

    Add files via upload

--- api/consulta.js
+++ api/consulta.js
@@ -17,20 +17,18 @@ export default async function handler(req, res) {
       const apiKey = process.env.AIRTABLE_API_KEY;
 
       const url = `https://api.airtable.com/v0/${baseId}/${tabela}?filterByFormula=AND({cnpj}='${cnpj}', {token}='${token}')`;
-console.log("Resposta do Airtable:", records);
 
       const resposta = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
+        headers: { Authorization: `Bearer ${apiKey}` }
       });
 
       const dados = await resposta.json();
+      console.log("Resposta do Airtable:", dados);
 
       if (dados.records && dados.records.length > 0) {
         res.status(200).json({ status: 'ok', parceiro: dados.records[0].fields });
       } else {
-        res.status(401).json({ status: 'erro', mensagem: 'Dados inválidos' });
+        res.status(401).json({ status: 'erro', mensagem: 'CNPJ ou Token inválidos' });
       }
     } catch (erro) {
       console.error('Erro interno:', erro);
